b92721e5dbdcc23b9c5a7ddc41391160eef38f0a
tcsinteractive <tcsinteractive@tcss-mac-mini>
2020-03-05 15:58:19 +0530

    ftu

--- src/test/java/com/test/flipkart/OrderItem.java
+++ src/test/java/com/test/flipkart/OrderItem.java
@@ -42,7 +42,7 @@ public class OrderItem {
 		try {
 			//Give IP of the internet
 			// Set Properties
-			System.out.println("Test Begins");
+			System.out.println("Test Begis");
 			gm = new GeneralMethods();
 			long starttime = System.currentTimeMillis();
 			startTimeString = String.valueOf(starttime);
